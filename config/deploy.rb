15ba0bf737cff14681b64b801b89c2d3409f3f10
blackbird07 <enrico.mrass@tippfuchs.de>
2012-04-27 20:50:11 +0200

    Fixed load order

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,5 @@
 require "bundler/capistrano"
 
-load "config/recipes/base"
-load "config/recipes/check"
-load "config/recipes/system_user"
-load "config/recipes/nodejs"
-#load "config/recipes/ssl"
-load "config/recipes/nginx"
-load "config/recipes/unicorn"
-load "config/recipes/mysql"
-load "config/recipes/rbenv"
-load "config/recipes/security"
-
-load "config/recipes/utilities"
-
-
 server "54.247.167.175", :web, :app, :db, primary: true
 set :server_name, "tippfuchs.de"
 set :application, "blog"
@@ -33,3 +19,18 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+# needs to be located after variable definitions, so that variables are available in loaded files
+
+load "config/recipes/base"
+load "config/recipes/check"
+load "config/recipes/system_user"
+load "config/recipes/nodejs"
+#load "config/recipes/ssl"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/mysql"
+load "config/recipes/rbenv"
+load "config/recipes/security"
+
+load "config/recipes/utilities"
